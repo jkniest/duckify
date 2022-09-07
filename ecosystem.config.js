7ffcf993d991d6872feac4aa313ebe4f3890dfa7
Jordan Kniest <mail@jkniest.de>
2022-09-08 01:07:31 +0200

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Duckify',
+            exec_mode: 'cluster',
+            instances: 'max',
+            script: './.output/server/index.mjs'
+        }
+    ]
+}
