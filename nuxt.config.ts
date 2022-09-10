c3c4f7e6670bb325f5d3dbc0fd35341eb5e296fa
Jordan Kniest <mail@jkniest.de>
2022-09-10 20:19:31 +0200

    Add spotify auth flow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@pinia/nuxt',
+        '@vueuse/nuxt'
+    ],
     ssr: false,
+    runtimeConfig: {
+        appUrl: process.env.APP_URL,
+        spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+        spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    }
 });
---
+++ server/api/spotify/authorize.ts
@@ -0,0 +1,38 @@
+import {defineEventHandler, sendRedirect} from "h3";
+import {useRuntimeConfig} from "#imports";
+
+export default defineEventHandler(async event => {
+    const url = new URL('http://localhost/' + event.req.url);
+    const code = url.searchParams.get('code');
+
+    const config = useRuntimeConfig();
+
+    const details = {
+        'grant_type': 'authorization_code',
+        code,
+        'redirect_uri': new URL('/api/spotify/authorize', config.appUrl).href
+    };
+
+    const formBody = [];
+    for (const property in details) {
+        formBody.push(encodeURIComponent(property) + "=" + encodeURIComponent(details[property]));
+    }
+    const formBodyStr = formBody.join("&");
+
+    const authHeader = new Buffer(
+        `${config.spotifyClientId}:${config.spotifyClientSecret}`
+    ).toString('base64');
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'post',
+        body: formBodyStr,
+        headers: {
+            Authorization: `Basic ${authHeader}`,
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        }
+    });
+
+    const json = await response.json();
+
+    return sendRedirect(event, `/sign-in?accessToken=${json['access_token']}&refreshToken=${json['refresh_token']}`);
+});
